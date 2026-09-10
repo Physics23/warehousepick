90631a50cefee79e0f0aedfcdd6b239f4f735305
AbdoulPhysics <dialloa626@yahoo.fr>
2026-09-10 17:19:42 -0500

    Fix CORS origin for actual Render frontend URL

--- warehousepick/settings.py
+++ warehousepick/settings.py
@@ -102,7 +102,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
-    "https://warehouse-kiosk-frontend.onrender.com",
+    "https://warehousepick-1.onrender.com"
 ]
 CORS_ALLOW_CREDENTIALS = True
 
