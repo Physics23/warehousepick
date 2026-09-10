cf5fe4fb3564c6e7b2a1e66a79a1167623ae3c0c
AbdoulPhysics <dialloa626@yahoo.fr>
2026-09-10 16:08:25 -0500

    Auto-create admin user in build script

--- build.sh
+++ build.sh
@@ -4,3 +4,13 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+# Create the admin user if it doesn't exist
+python manage.py shell -c "
+from django.contrib.auth.models import User
+if not User.objects.filter(username='alaye').exists():
+    User.objects.create_superuser('alaye', '', '123')
+    print('Created admin user: alaye')
+else:
+    print('Admin user alaye already exists')
+"
