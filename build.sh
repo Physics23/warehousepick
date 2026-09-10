92c42da88ff4700fa47919fec79049d62668ed42
AbdoulPhysics <dialloa626@yahoo.fr>
2026-09-10 18:06:18 -0500

    Auto-create station and totes on deploy

--- build.sh
+++ build.sh
@@ -1,17 +1,28 @@
 #!/usr/bin/env bash
+#!/usr/bin/env bash
 set -o errexit
 
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
 
-# Always create or reset the admin user (idempotent, safe to run every deploy)
+# Create the admin user (idempotent)
 python manage.py shell -c "
 from django.contrib.auth.models import User
-user, created = User.objects.get_or_create(username='alaye')
+user, _ = User.objects.get_or_create(username='alaye')
 user.set_password('123')
 user.is_staff = True
 user.is_superuser = True
 user.save()
-print('Admin user alaye: password set to 123 (created=' + str(created) + ')')
+print('Admin user alaye ready')
+"
+
+# Create the station and totes (idempotent)
+python manage.py shell -c "
+from picks.models import Station, Tote
+s, c1 = Station.objects.get_or_create(station_id='PickstationASRS4159', defaults={'name': 'Pick Station'})
+t1, c2 = Tote.objects.get_or_create(tote_id='BIN-01')
+t2, c3 = Tote.objects.get_or_create(tote_id='TOTE-42')
+print(f'Station PickstationASRS4159: {\"created\" if c1 else \"exists\"}')
+print(f'Totes BIN-01/TOTE-42: {\"created\" if c2 else \"exists\"}/{\"created\" if c3 else \"exists\"}')
 "
